2090d4923041852ce8a6a700fe8850e32ed22d5c
sanfromwebsite <sanloun12@gmail.com>
2026-02-24 15:00:48 +0700

    [Update] update to can post to timeLine

--- wpf_ui/ToolLib/Data/SQLConstant.cs
+++ wpf_ui/ToolLib/Data/SQLConstant.cs
@@ -5,11 +5,19 @@ namespace ToolLib.Data
     public partial class SQLConstant
     {
         public const string DATABASE_NAME = "KhBrowser";
-        public const string CONNECTION_STRING = "Data Source=192.168.69.83\\MSSQLSERVER23;Initial Catalog=KhBrowser;user id=sothea;password=Thea8989;Connection Timeout=30;Pooling=true;Max Pool Size=200;Encrypt=False;TrustServerCertificate=true;MultipleActiveResultSets=True;";
-        //public const string CONNECTION_STRING ="Data Source=localhost;Persist Security Info=True;User ID=david;Password=David@123;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Application Name=\"SQL Server Management Studio\";";
 
-        //public const string MASTER_CONNECTION_STRING = "Data Source=localhost;Persist Security Info=True;User ID=david;Password=David@123;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Application Name=\"SQL Server Management Studio\";";
+        ///// [Work Local]
+        //public const string CONNECTION_STRING = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=KhBrowser;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;Application Name=\"ToolKHBrowser\";";
+        //public const string MASTER_CONNECTION_STRING = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;Application Name=\"ToolKHBrowser\";";
+
+
+        ///// [Server Test]
+        public const string CONNECTION_STRING = "Data Source=192.168.69.83\\MSSQLSERVER23;Initial Catalog=KhBrowser;user id=sothea;password=Thea8989;Connection Timeout=30;Pooling=true;Max Pool Size=200;Encrypt=False;TrustServerCertificate=true;MultipleActiveResultSets=True;";
         public const string MASTER_CONNECTION_STRING = "Data Source=192.168.69.83\\MSSQLSERVER23;Initial Catalog=master;user id=sothea;password=Thea8989;Connection Timeout=30;Pooling=true;Max Pool Size=200;Encrypt=False;TrustServerCertificate=true;MultipleActiveResultSets=True;";
+
+        ///// [David Home Test]
+        //public const string CONNECTION_STRING ="Data Source=localhost;Persist Security Info=True;User ID=david;Password=David@123;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Application Name=\"SQL Server Management Studio\";";
+        //public const string MASTER_CONNECTION_STRING = "Data Source=localhost;Persist Security Info=True;User ID=david;Password=David@123;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Application Name=\"SQL Server Management Studio\";";
     }
 }
 
